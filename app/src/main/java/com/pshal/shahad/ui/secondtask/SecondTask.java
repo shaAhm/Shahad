874f374c0225e832cf9887c1bb8d06694af89fc8
96653 <shahdalshehri.1996@gmail.com>
2021-10-18 21:53:41 +0300

    First App

---
+++ app/src/main/java/com/pshal/shahad/ui/secondtask/SecondTask.java
@@ -0,0 +1,16 @@
+package com.pshal.shahad.ui.secondtask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pshal.shahad.R;
+
+public class SecondTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_task);
+    }
+}
